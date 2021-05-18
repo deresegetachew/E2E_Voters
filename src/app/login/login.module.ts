ecd5ce1f9b8a6152f582f1e937557b1a9d8d1b7d
deresseg <deresse.getachew@omnicell.com>
2021-05-18 17:05:21 +0300

    login components

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginPageComponent } from './container/login-page/login-page.component';
+import { LoginComponent } from './component/login/login.component';
+import { LoginFormComponent } from './component/login-form/login-form.component';
+import { LoginSocialComponent } from './component/login-social/login-social.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+    LoginComponent,
+    LoginFormComponent,
+    LoginSocialComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
